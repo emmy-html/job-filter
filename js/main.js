dffbf5a0961c2fde37097d7b668f88d3c5217b07
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-07 21:55:37 -0500

    Updated script so that it works when using div IDs

--- js/main.js
+++ js/main.js
@@ -1,21 +1,45 @@
 function jobFilter() {
-    var frontEnd = document.getElementById("front-end-job");
-  var css =
-  document.getElementById("css-job");
-  var js =
-  document.getElementById("js-job");
-  
-  var frontEndFilter = document.getElementById("front-end-button");
-  var cssFilter = document.getElementById("css-button");
-  var jsFilter = document.getElementById("js-button");
-  
-  var test = document.getElementById("testing-content");
-    if(jsFilter.checked === true) {
-      frontEnd.style.display = "none";
-      css.style.display = "none";
-      js.style.display = "inherit";
-    } else {
-      css.style.display = "none";
-      js.style.display = "none";
-    }
+  var frontEndJobs = document.getElementById("front-end-job");
+var cssJobs =
+document.getElementById("css-job");
+var jsJobs =
+document.getElementById("js-job");
+
+var frontEndFilter = document.getElementById("front-end-button").checked;
+var cssFilter = document.getElementById("css-button").checked;
+var jsFilter = document.getElementById("js-button").checked;
+
+  if(frontEndFilter === true && cssFilter === true && jsFilter === true) {
+    frontEndJobs.style.display = "block";
+    cssJobs.style.display = "block";
+    jsJobs.style.display = "block";
+  } else if (frontEndFilter === true && cssFilter === false && jsFilter === false) {
+    frontEndJobs.style.display = "block";
+    cssJobs.style.display = "none";
+    jsJobs.style.display = "none"; 
+  } else if (frontEndFilter === false && cssFilter === true && jsFilter === false) {
+    frontEndJobs.style.display = "none";
+    cssJobs.style.display = "block";
+    jsJobs.style.display = "none"; 
+  } else if (frontEndFilter === false && cssFilter === false && jsFilter === true) {
+    frontEndJobs.style.display = "none";
+    cssJobs.style.display = "none";
+    jsJobs.style.display = "block"; 
+  } else if (frontEndFilter === true && cssFilter === true && jsFilter === false) {
+    frontEndJobs.style.display = "block";
+    cssJobs.style.display = "block";
+    jsJobs.style.display = "none"; 
+  } else if (frontEndFilter === false && cssFilter === true && jsFilter === true) {
+    frontEndJobs.style.display = "none";
+    cssJobs.style.display = "block";
+    jsJobs.style.display = "block"; 
+  } else if (frontEndFilter === true && cssFilter === false && jsFilter === true) {
+    frontEndJobs.style.display = "block";
+    cssJobs.style.display = "none";
+    jsJobs.style.display = "block"; 
+  } else {
+    frontEndJobs.style.display = "block";
+    cssJobs.style.display = "block";
+    jsJobs.style.display = "block"; 
   }
+}
