d76395972d157d1943ffbb064077e8401d7053d2
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-07 23:11:47 -0500

    Updated if/else statements to make the buttons fully functional

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
-// loop to list all jobs by type (?)
+// loop to list all jobs by type
 function listJobs(type) {
   var i;
   for (i = 0; i < type.length; i++) {
     type[i].style.display = "block";
   }
 }
-// loop to hide all jobs by type (?)
+// loop to hide all jobs by type
 function hideJobs(type) {
   var i;
   for (i = 0; i < type.length; i++) {
@@ -23,9 +23,37 @@ function jobFilter() {
   var cssFilter = document.getElementById("css-button").checked;
   var jsFilter = document.getElementById("js-button").checked;
   // if/else statements that determine which jobs are shown
-  if (frontEndFilter === true) {
+  if (frontEndFilter === true && cssFilter === true && jsFilter === true) {
     listJobs(frontEndJobs);
-  } else {
+    listJobs(cssJobs);
+    listJobs(jsJobs);
+  } else if (frontEndFilter === true && cssFilter === false && jsFilter === false) {
+    listJobs(frontEndJobs);
+    hideJobs(cssJobs);
+    hideJobs(jsJobs);
+  } else if (frontEndFilter === false && cssFilter === true && jsFilter === false) {
     hideJobs(frontEndJobs);
+    listJobs(cssJobs);
+    hideJobs(jsJobs);
+  } else if (frontEndFilter === false && cssFilter === false && jsFilter === true) {
+    hideJobs(frontEndJobs);
+    hideJobs(cssJobs);
+    listJobs(jsJobs);
+  } else if (frontEndFilter === true && cssFilter === true && jsFilter === false) {
+    listJobs(frontEndJobs);
+    listJobs(cssJobs);
+    hideJobs(jsJobs);
+  } else if (frontEndFilter === false && cssFilter === true && jsFilter === true) {
+    hideJobs(frontEndJobs);
+    listJobs(cssJobs);
+    listJobs(jsJobs);
+  } else if (frontEndFilter === true && cssFilter === false && jsFilter === true) {
+    listJobs(frontEndJobs);
+    hideJobs(cssJobs);
+    listJobs(jsJobs);
+  } else {
+    listJobs(frontEndJobs);
+    listJobs(cssJobs);
+    listJobs(jsJobs);
   }
 }
