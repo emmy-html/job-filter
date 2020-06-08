167dd756d170720d2a6f1232a64b1bd092403893
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-07 22:37:17 -0500

    very basic functionality added with use of class names & for loops

--- js/main.js
+++ js/main.js
@@ -1,45 +1,31 @@
+// loop to list all jobs by type (?)
+function listJobs(type) {
+  var i;
+  for (i = 0; i < type.length; i++) {
+    type[i].style.display = "block";
+  }
+}
+// loop to hide all jobs by type (?)
+function hideJobs(type) {
+  var i;
+  for (i = 0; i < type.length; i++) {
+    type[i].style.display = "none";
+  }
+}
+// function that filters jobs when checkboxes are clicked
 function jobFilter() {
-  var frontEndJobs = document.getElementById("front-end-job");
-var cssJobs =
-document.getElementById("css-job");
-var jsJobs =
-document.getElementById("js-job");
-
-var frontEndFilter = document.getElementById("front-end-button").checked;
-var cssFilter = document.getElementById("css-button").checked;
-var jsFilter = document.getElementById("js-button").checked;
-
-  if(frontEndFilter === true && cssFilter === true && jsFilter === true) {
-    frontEndJobs.style.display = "block";
-    cssJobs.style.display = "block";
-    jsJobs.style.display = "block";
-  } else if (frontEndFilter === true && cssFilter === false && jsFilter === false) {
-    frontEndJobs.style.display = "block";
-    cssJobs.style.display = "none";
-    jsJobs.style.display = "none"; 
-  } else if (frontEndFilter === false && cssFilter === true && jsFilter === false) {
-    frontEndJobs.style.display = "none";
-    cssJobs.style.display = "block";
-    jsJobs.style.display = "none"; 
-  } else if (frontEndFilter === false && cssFilter === false && jsFilter === true) {
-    frontEndJobs.style.display = "none";
-    cssJobs.style.display = "none";
-    jsJobs.style.display = "block"; 
-  } else if (frontEndFilter === true && cssFilter === true && jsFilter === false) {
-    frontEndJobs.style.display = "block";
-    cssJobs.style.display = "block";
-    jsJobs.style.display = "none"; 
-  } else if (frontEndFilter === false && cssFilter === true && jsFilter === true) {
-    frontEndJobs.style.display = "none";
-    cssJobs.style.display = "block";
-    jsJobs.style.display = "block"; 
-  } else if (frontEndFilter === true && cssFilter === false && jsFilter === true) {
-    frontEndJobs.style.display = "block";
-    cssJobs.style.display = "none";
-    jsJobs.style.display = "block"; 
+  // selects all elements with class name corresponding with job type
+  var frontEndJobs = document.querySelectorAll(".front-end-job");
+  var cssJobs = document.querySelectorAll(".css-job");
+  var jsJobs = document.querySelectorAll(".js-job");
+  // selects checkboxes for job type
+  var frontEndFilter = document.getElementById("front-end-button").checked;
+  var cssFilter = document.getElementById("css-button").checked;
+  var jsFilter = document.getElementById("js-button").checked;
+  // if/else statements that determine which jobs are shown
+  if (frontEndFilter === true) {
+    listJobs(frontEndJobs);
   } else {
-    frontEndJobs.style.display = "block";
-    cssJobs.style.display = "block";
-    jsJobs.style.display = "block"; 
+    hideJobs(frontEndJobs);
   }
 }
