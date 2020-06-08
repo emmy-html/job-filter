f9a1bbc1882fb3fe7bff9ad01c4276f10c91f826
Emmy <60683173+emmy-html@users.noreply.github.com>
2020-06-07 23:20:29 -0500

    Added functionality to the 'clear' button & formatted with Prettier

--- js/main.js
+++ js/main.js
@@ -1,23 +1,23 @@
-// loop to list all jobs by type
+// loop to list all jobs by type (?)
 function listJobs(type) {
   var i;
   for (i = 0; i < type.length; i++) {
     type[i].style.display = "block";
   }
 }
-// loop to hide all jobs by type
+// loop to hide all jobs by type (?)
 function hideJobs(type) {
   var i;
   for (i = 0; i < type.length; i++) {
     type[i].style.display = "none";
   }
 }
+// selects all elements with class name corresponding with job type
+let frontEndJobs = document.querySelectorAll(".front-end-job");
+let cssJobs = document.querySelectorAll(".css-job");
+let jsJobs = document.querySelectorAll(".js-job");
 // function that filters jobs when checkboxes are clicked
 function jobFilter() {
-  // selects all elements with class name corresponding with job type
-  var frontEndJobs = document.querySelectorAll(".front-end-job");
-  var cssJobs = document.querySelectorAll(".css-job");
-  var jsJobs = document.querySelectorAll(".js-job");
   // selects checkboxes for job type
   var frontEndFilter = document.getElementById("front-end-button").checked;
   var cssFilter = document.getElementById("css-button").checked;
@@ -27,27 +27,51 @@ function jobFilter() {
     listJobs(frontEndJobs);
     listJobs(cssJobs);
     listJobs(jsJobs);
-  } else if (frontEndFilter === true && cssFilter === false && jsFilter === false) {
+  } else if (
+    frontEndFilter === true &&
+    cssFilter === false &&
+    jsFilter === false
+  ) {
     listJobs(frontEndJobs);
     hideJobs(cssJobs);
     hideJobs(jsJobs);
-  } else if (frontEndFilter === false && cssFilter === true && jsFilter === false) {
+  } else if (
+    frontEndFilter === false &&
+    cssFilter === true &&
+    jsFilter === false
+  ) {
     hideJobs(frontEndJobs);
     listJobs(cssJobs);
     hideJobs(jsJobs);
-  } else if (frontEndFilter === false && cssFilter === false && jsFilter === true) {
+  } else if (
+    frontEndFilter === false &&
+    cssFilter === false &&
+    jsFilter === true
+  ) {
     hideJobs(frontEndJobs);
     hideJobs(cssJobs);
     listJobs(jsJobs);
-  } else if (frontEndFilter === true && cssFilter === true && jsFilter === false) {
+  } else if (
+    frontEndFilter === true &&
+    cssFilter === true &&
+    jsFilter === false
+  ) {
     listJobs(frontEndJobs);
     listJobs(cssJobs);
     hideJobs(jsJobs);
-  } else if (frontEndFilter === false && cssFilter === true && jsFilter === true) {
+  } else if (
+    frontEndFilter === false &&
+    cssFilter === true &&
+    jsFilter === true
+  ) {
     hideJobs(frontEndJobs);
     listJobs(cssJobs);
     listJobs(jsJobs);
-  } else if (frontEndFilter === true && cssFilter === false && jsFilter === true) {
+  } else if (
+    frontEndFilter === true &&
+    cssFilter === false &&
+    jsFilter === true
+  ) {
     listJobs(frontEndJobs);
     hideJobs(cssJobs);
     listJobs(jsJobs);
@@ -56,4 +80,13 @@ function jobFilter() {
     listJobs(cssJobs);
     listJobs(jsJobs);
   }
+}
+// reset selected options on click
+function clearResults() {
+  document.getElementById("front-end-button").checked = false;
+  document.getElementById("css-button").checked = false;
+  document.getElementById("js-button").checked = false;
+  listJobs(frontEndJobs);
+  listJobs(cssJobs);
+  listJobs(jsJobs);
 }
